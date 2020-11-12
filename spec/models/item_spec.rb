d393344de4a9aaeb582b662bdade921ebe81f528
Nakamizo0606 <52415241t@gmail.com>
2020-11-12 15:10:42 +0900

    修正3

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -24,37 +24,37 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Info can't be blank")
     end
     
-    it "カテゴリーの情報が必須であること" do
+    it "カテゴリーの情報が(---)のだと保存できない" do
       @item.category_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include("Category can't be blank")
     end
     
-    it "商品の状態についての情報が必須であること" do
+    it "商品の状態についての情報が(---)の必須であること" do
       @item.sales_status_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include("Sales status can't be blank")
     end
     
-    it "配送料の負担についての情報が必須であること" do
+    it "配送料の負担についての情報が(---)の必須であること" do
       @item.shipping_fee_status_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping fee status can't be blank")
     end
     
-    it "発送元の地域についての情報が必須であること" do
+    it "発送元の地域についての情報が(---)の必須であること" do
       @item.prefecture_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include("Prefecture can't be blank")
     end
     
-    it "発送までの日数についての情報が必須であること" do
+    it "発送までの日数についての情報が(---)の必須であること" do
       @item.scheduled_delivery_id = 0
       @item.valid?
       expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank")
     end
     
-    it "価格についての情報が必須であること" do
+    it "価格についての情報が(---)の必須であること" do
       @item.price = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Price can't be blank")
